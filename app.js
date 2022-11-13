152990201447b700477ccd4ca54bbe2b27e56c4b
Onyeriri Victor <viktorchinonso@gmail.com>
2022-11-13 10:31:07 +0000

    Completed node core model assessment

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
-const names = require("./person");
+const os = require("os");
+const path = require("path");
+// const names = require("./person");
 
-// const greet = (name) => {
-//   console.log(name);
-// };
-console.log(names);
-// greet("victor");
-// greet("Florence");
+// // const greet = (name) => {
+// //   console.log(name);
+// // };
+// console.log(names);
+// // greet("victor");
+// // greet("Florence");
 
-// console.log(globalThis);
-// let counter = 0;
+// // console.log(globalThis);
+// // let counter = 0;
 
-// const clear = globalThis.setInterval(() => {
-//   console.log("MERN stack course");
-//   counter++;
+// // const clear = globalThis.setInterval(() => {
+// //   console.log("MERN stack course");
+// //   counter++;
 
-//   if (counter === 10) {
-//     console.log(counter);
-//     globalThis.clearInterval(clear);
-//   }
-// }, 1000);
+// //   if (counter === 10) {
+// //     console.log(counter);
+// //     globalThis.clearInterval(clear);
+// //   }
+// // }, 1000);
 
-// console.log(__dirname);
-// console.log(__filename);
+// // console.log(__dirname);
+// // console.log(__filename);
+
+console.log(os.platform());
+console.log(path);
